60a8d503939475bc467124856fdbf232f8a23546
Tkariclook <tkariclook@gmail.com>
2020-10-29 20:36:27 +0300

    chapter 3 exe

--- src/main/scala/chapter3/ExercisesWorkingWithArrays.scala
+++ src/main/scala/chapter3/ExercisesWorkingWithArrays.scala
@@ -27,15 +27,13 @@ object ExercisesWorkingWithArrays extends App {
   }
   println(array0)
   //3
-  val array1 = ArrayBuffer(1, 2, 3, 4, 5, 6, 7)
+  val array1 = ArrayBuffer(1, 2, 3, 4, 5, 6, 7,8)
   println(array1)
   val newArr = for (i <- 1 to array1.length / 2 * 2 - 1 by 2;
                                          j <- i - 1 to i) yield {
     if (i > j && (i - 1) == j) {
-      println(s"$i $j = ${array1(i)}")
       array1(i)
     } else if (i == j) {
-      println(s"$i $j = ${array1(i - 1)}")
       array1(i - 1)
     }
   }
@@ -43,7 +41,7 @@ object ExercisesWorkingWithArrays extends App {
     println( newArr ++ array1.reverse.take(1))
   }
   println(array1)
-  val newArr1 = for (i <- 1 until  array1.length + 1 by 2 ;
+  val newArr1 = for (i <- 1 to  array1.length + 1 by 2 ;
                      j <- i to i-1 by -1 if j < array1.length)
     yield  array1(j)
 
