29169d4e0999a0a714ad49bf8ec0e91c9e01231f
Tkariclook <tkariclook@gmail.com>
2020-11-06 16:19:47 +0300

    chapter5 exercise8

--- src/main/scala/chapter5/exercises.scala
+++ src/main/scala/chapter5/exercises.scala
@@ -123,4 +123,30 @@ object exercises extends App{
   }
   val ex7 = new exercise7.Person("Fred Smith")
   println(s"${ex7.firstName}  ${ex7.lastName} ")
+  object  exercise8 {
+    /*
+    * Make a class Car with read-only properties for manufacturer, model name, and model year,
+    * and a read-write property for the license plate. Supply four constructors. All require the
+    * manufacturer and model name. Optionally, model year and license plate can also be specified in
+    * the constructor. If not, the model year is set to -1 and the license plate to the empty string.
+    * Which constructor are you choosing as the primary constructor? Why?
+    */
+    class Car(val manufacturer: String, val modelName: String, val modelYear: Int, val licensePlate: String) {
+
+      def this(manufacturer: String, modelName: String) {
+        this(manufacturer, modelName, -1, "")
+      }
+
+      def this(manufacturer: String, modelName: String, modelYear: Int) {
+        this(manufacturer, modelName, modelYear, "")
+      }
+
+      def this(manufacturer: String, modelName: String, licensePlate: String) {
+        this(manufacturer, modelName, -1, licensePlate)
+      }
+    }
+
+  }
+
+  val ex8 = new exercise8.Car("df", "ds", 1999, "888")
 }
