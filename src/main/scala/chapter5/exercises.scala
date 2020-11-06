87db22146ef38db04ad4350af8cda1dc154b247b
Tkariclook <tkariclook@gmail.com>
2020-11-06 15:16:48 +0300

    chapter5 exercise6

--- src/main/scala/chapter5/exercises.scala
+++ src/main/scala/chapter5/exercises.scala
@@ -100,4 +100,14 @@ object exercises extends App{
   st0.setId(52222)
   st0.setName("testStudent")
   println(st0.getId + " " + st0.getName)
+
+  object exercise6 {
+    /* In the Person class of Section 5.1, “Simple Classes and Parameterless Methods,” on page 55,
+     * provide a primary constructor that turns negative ages to 0.
+     */
+    class Person(var age: Int) {
+      if (age < 0) age = 0
+    }
+  }
+
 }
