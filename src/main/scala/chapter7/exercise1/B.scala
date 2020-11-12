b4f53644467a54d1257b13b09da0796df4250323
Tkariclook <tkariclook@gmail.com>
2020-11-12 17:50:08 +0300

    chapter 7 exercise 1

---
+++ src/main/scala/chapter7/exercise1/B.scala
@@ -0,0 +1,7 @@
+package chapter7.exercise1.com {
+  object B { val B = "b0"}
+  package horstmann {
+    object B { val B = "b1"}
+  }
+
+}
---
+++ src/main/scala/chapter7/exercise1/Exercises1.scala
@@ -0,0 +1,7 @@
+package chapter7.exercise1
+
+object Exercises1 extends App{
+  println(com.horstmann.impatient.A.A)
+  println(com.horstmann.B.B)
+  println(com.B.B)
+}
