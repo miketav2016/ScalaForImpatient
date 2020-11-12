123f410d18114350f26c2d4d5dece6dc750f1ff0
Tkariclook <tkariclook@gmail.com>
2020-11-12 19:27:07 +0300

    chapter 7 exercise 9

---
+++ src/main/scala/chapter7/exercise9/ex9.scala
@@ -0,0 +1,16 @@
+package chapter7.exercise9
+/**Write a program that imports the java.lang.System class, reads the user name from the
+user.name system property, reads a password from the StdIn object, and prints a message
+to the standard error stream if the password is not "secret". Otherwise, print a greeting to
+the standard output stream. Do not use any other imports, and do not use any qualified names
+(with dots).
+*/
+import  java.lang.System.{getProperties => prop}
+object ex9 extends App {
+  val res = prop.get("user.name")
+  val password = "secret"
+  if(scala.io.StdIn.readLine().equals(password))
+    println(res)
+  else
+   println("Wrong password")
+}
