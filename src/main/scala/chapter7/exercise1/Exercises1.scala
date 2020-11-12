25681973ddce955adce1ee2d2e2919ffc277ad22
Tkariclook <tkariclook@gmail.com>
2020-11-12 18:01:20 +0300

    chapter 7 exercise 2

--- src/main/scala/chapter7/exercise1/Exercises1.scala
+++ src/main/scala/chapter7/exercise1/Exercises1.scala
@@ -1,5 +1,10 @@
 package chapter7.exercise1
-
+/**Write an example program to demonstrate that
+package com.horstmann.impatient
+is not the same as
+package com
+package horstmann
+ */
 object Exercises1 extends App{
   println(com.horstmann.impatient.A.A)
   println(com.horstmann.B.B)
---
+++ src/main/scala/chapter7/exercise2/puzzel/scala/com/Friend.scala
@@ -0,0 +1,5 @@
+package chapter7.exercise2.puzzel.scala.com {
+  object Friend {
+    /** Write a puzzler that baffles your Scala friends, using a package com that isn’t at the top level. */
+  }
+}
