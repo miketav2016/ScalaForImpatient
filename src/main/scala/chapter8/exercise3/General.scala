cdd2ed5e24c0531e77e4165aec27ce9d750ac02d
Tkariclook <tkariclook@gmail.com>
2020-11-15 20:49:28 +0300

    chapter 8 exercise 3

---
+++ src/main/scala/chapter8/exercise3/General.scala
@@ -0,0 +1,9 @@
+package chapter8.exercise3
+
+class General
+class Alpha extends General
+class Betta extends Alpha
+class BettaPlus extends Alpha
+class BettaMinus extends Betta
+class Gamma extends Betta
+class Other extends Gamma
