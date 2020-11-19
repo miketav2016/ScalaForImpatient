a9286ba90935cefadc91184e192eee1a7a7c73f9
Tkariclook <tkariclook@gmail.com>
2020-11-19 19:28:31 +0300

    CH9 exe4

---
+++ src/main/scala/chapter9/exercises/exercise4/ReadDouble.scala
@@ -0,0 +1,18 @@
+package chapter9.exercises.exercise4
+
+import scala.io.{BufferedSource, Source}
+
+object ReadDouble extends App {
+  def statistic(path: String): Unit = {
+    val source: BufferedSource = Source.fromFile(path, "UTF-8")
+    val tokens: Array[String] = source.mkString.split("\\s+")
+    val doubles: Array[Double] = tokens.map(_.toDouble)
+    println(s"sum = ${doubles.sum}")
+    println(s"avr = ${doubles.sum / doubles.length}")
+    println(s"max = ${doubles.max}")
+    println(s"min = ${doubles.min}")
+  }
+
+  val path = "src/main/scala/chapter9/exercises/exercise4/test.txt"
+  statistic(path)
+}
