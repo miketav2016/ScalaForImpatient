adf8141acf5a5e92f9a87abd96d615b89a6cf131
Tkariclook <tkariclook@gmail.com>
2020-11-21 09:46:50 +0300

    CH9EX8

---
+++ src/main/scala/chapter9/exercises/exercise8/PrintsTheSrcAttributes.scala
@@ -0,0 +1,17 @@
+package chapter9.exercises.exercise8
+
+import scala.io.{BufferedSource, Source}
+
+object PrintsTheSrcAttributes extends App {
+  def function(): Unit = {
+    val imgRegex = "<img.*src=(?:\"|')([^\"']+).*>".r
+    val path = "https://stackoverflow.com/questions/1196570/using-regular-expressions-to-extract-the-first-image-source-from-html-codes"
+    val source: BufferedSource = Source.fromURL(path, "UTF-8")
+    val tokens: Array[String] = source.mkString.split("\\s+")
+    for (imgRegex(src) <- imgRegex.findAllIn(tokens.mkString))
+      println(src)
+    source.close()
+  }
+
+  function()
+}
